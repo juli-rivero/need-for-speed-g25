5646d56d094876b21857f48efa5755db11a27dab
lucchesifd <lucchesifd@gmail.com>
2025-11-18 01:28:12 -0300

    Mejoras graficas varias

--- src/client/game/car.h
+++ src/client/game/car.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL2pp/SDL2pp.hh>
+#include <string>
 
 #include "client/game/game.h"
 #include "client/game/mock_api.h"
@@ -14,20 +15,26 @@ class Car final {
 
     // Parametros mutables
     int id;
+    std::string name;
     float x;
     float y;
     float angle;
     float speed;
+    float health;
     SDL2pp::Texture& sprite;
+
+    int get_vol(double x, double y);
 
    public:
     Car(Game& game, const PlayerSnapshot& base);
 
     void set_camera();
     void sound_crash();
-    void draw();
+    void draw(bool with_name);
 
     size_t get_id();
+    float get_health();
+    float get_speed();
 
     MAKE_FIXED(Car)
 };
--- src/client/game/game.cpp
+++ src/client/game/game.cpp
@@ -28,9 +28,18 @@ void Game::render(const std::string& texto, int x, int y, bool in_world) {
     render(t, x, y, 0, in_world);
 }
 
+void Game::render_rect(SDL2pp::Rect rect, const SDL2pp::Color& color,
+                       bool in_world) {
+    if (in_world) rect -= SDL2pp::Point(cam_x, cam_y);
+
+    renderer.SetDrawColor(color).FillRect(rect);
+}
+
 //
 // PRINCIPAL
 //
+
+#define MY_ID 0
 
 bool Game::send_events() {
     SDL_Event event;
@@ -42,10 +51,12 @@ bool Game::send_events() {
 
             if (tecla == SDLK_q || tecla == SDLK_ESCAPE) return true;
 
-            if (tecla == SDLK_LEFT) api.start_turning(TurnDirection::Left);
-            if (tecla == SDLK_RIGHT) api.start_turning(TurnDirection::Right);
-            if (tecla == SDLK_UP) api.start_accelerating();
-            if (tecla == SDLK_DOWN) api.start_breaking();
+            if (tecla == SDLK_LEFT || tecla == SDLK_a)
+                api.start_turning(TurnDirection::Left);
+            if (tecla == SDLK_RIGHT || tecla == SDLK_d)
+                api.start_turning(TurnDirection::Right);
+            if (tecla == SDLK_UP || tecla == SDLK_w) api.start_accelerating();
+            if (tecla == SDLK_DOWN || tecla == SDLK_s) api.start_breaking();
 
             if (tecla == SDLK_SPACE) api.start_using_nitro();
         }
@@ -53,10 +64,12 @@ bool Game::send_events() {
         if (event.type == SDL_KEYUP) {
             auto tecla = event.key.keysym.sym;
 
-            if (tecla == SDLK_LEFT) api.stop_turning(TurnDirection::Left);
-            if (tecla == SDLK_RIGHT) api.stop_turning(TurnDirection::Right);
-            if (tecla == SDLK_UP) api.stop_accelerating();
-            if (tecla == SDLK_DOWN) api.stop_breaking();
+            if (tecla == SDLK_LEFT || tecla == SDLK_a)
+                api.stop_turning(TurnDirection::Left);
+            if (tecla == SDLK_RIGHT || tecla == SDLK_d)
+                api.stop_turning(TurnDirection::Right);
+            if (tecla == SDLK_UP || tecla == SDLK_w) api.stop_accelerating();
+            if (tecla == SDLK_DOWN || tecla == SDLK_s) api.stop_breaking();
         }
     }
 
@@ -66,11 +79,11 @@ bool Game::send_events() {
 void Game::get_state() {
     cars.clear();
 
-    for (auto& player : api.get_snapshot().players)
+    for (auto& player : api.get_snapshot().players) {
         cars.emplace_back(*this, player);
+        if (player.id == MY_ID) my_car = &cars.back();
+    }
 }
-
-#define MY_ID 0
 
 void Game::draw_state() {
     renderer.Clear();
@@ -79,21 +92,29 @@ void Game::draw_state() {
     render(assets.city_liberty, 0, 0);
 
     // Coches
+    my_car->set_camera();
     for (Car& car : cars) {
-        if (car.get_id() == MY_ID) car.set_camera();
-        car.draw();
+        if (&car == my_car) continue;
+        car.draw(true);
     }
 
-    // HUD
-    render("Hola, mundo!", 10, 10, false);
+    my_car->draw(false);
 
+    // HUD
+    // render("Hola, mundo!", 10, 10, false);
+    render_rect({10, 10, static_cast<int>(my_car->get_health()), 10},
+                {0, 255, 0, 255}, false);
+    render_rect({10, 30, static_cast<int>(my_car->get_speed()), 10},
+                {255, 165, 0, 255}, false);
+
+    renderer.SetDrawColor(0, 0, 0, 255);
     renderer.Present();
 }
 
 void Game::play_sounds() {
-    for (Car& car : cars) {
-        if (car.get_id() == 1) car.sound_crash();
-    }
+    // for (Car& car : cars) {
+    //     if (car.get_id() == 1) car.sound_crash();
+    // }
 }
 
 void Game::wait_next_frame() {
