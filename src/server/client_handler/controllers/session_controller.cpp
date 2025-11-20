e65d2587c496639601cbffb3a57b7fff91a176a7
juli-rivero <jarivero@fi.uba.ar>
2025-11-20 00:16:02 -0300

    fix(connexion): desconectando y conectando clientes no crashea
    Antes, cuando se desconectaba un cliente, el servidor se crasheaba.
    Se detectó que el socket no detectaba que estaba cerrado cuando el cliente lo cerró,
    asi que luego se cerraba nuevamente y crasheaba.
    
    Ahora, como agregado, se ve reflejado que el jugador se salio de la session
    si se desconectó, y no esta en partida sino en la sala de espera.

--- src/server/client_handler/controllers/session_controller.cpp
+++ src/server/client_handler/controllers/session_controller.cpp
@@ -19,17 +19,19 @@ SessionController::SessionController(Session& session, const PlayerId client_id,
       api(api),
       dispatcher(handler),
       session(session) {
-    log->debug("controlling lobby");
     Receiver::Listener::subscribe(receiver);
     Session::Listener::subscribe(session);
+    session.add_client(client_id);
     api.reply_joined(session.get_info(), session.get_types_of_static_cars());
+    log->debug("controlling session");
 }
 
 SessionController::~SessionController() {
     Receiver::Listener::unsubscribe();
     Session::Listener::unsubscribe();
-    log->trace("left session");
     api.reply_left();
+    session.remove_client(client_id);
+    log->trace("left session");
 }
 
 void SessionController::on_start_request(const bool ready) {
@@ -52,7 +54,6 @@ void SessionController::on_choose_car(const std::string& car_name) {
 }
 void SessionController::on_leave_request() {
     try {
-        session.remove_client(client_id);
         dispatcher.on_leave_session();
     } catch (std::exception& e) {
         log->warn("could not send session snapshot: {}", e.what());
