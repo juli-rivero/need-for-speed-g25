35aecc8f9fcaee5334c5ba7c8bc9f39f17b2c4a4
lucchesifd <lucchesifd@gmail.com>
2025-11-30 21:59:04 -0300

    Sistema de sonido

--- src/client/game/classes.h
+++ src/client/game/classes.h
@@ -24,6 +24,7 @@ class Screen final {
    private:
     SDL2pp::Renderer& renderer;
     Game& game;
+    AssetsScreen assets;
 
     PlayerCar* my_car = nullptr;
 
@@ -68,7 +69,7 @@ class Screen final {
     void render_car(PlayerCar& car, bool with_name);
 
    public:
-    Screen(SDL2pp::Renderer& renderer, Game& game);
+    Screen(SDL2pp::Renderer& renderer, Game& game, const CityName& city_name);
 
     void update();
 
@@ -79,67 +80,59 @@ class Sound final {
    private:
     SDL2pp::Mixer& mixer;
     Game& game;
+    AssetsSound assets;
 
-    bool old_braking = false;
-    bool old_finished = false;
+    // Manejo interno de sonidos
+    struct SoundInfo {
+        PlayerId from;
+        int channel_id;
+    };
+    std::list<SoundInfo> brakes;
+    std::list<SoundInfo> crashes;
 
+    // Reproduccion de efectos
+    void try_brake(const PlayerCar& car);
+    void try_crash(const PlayerCar& car);
+    void play_checkpoint();
+    void play_goal();
+
+    // Finalizacion interna (por update)
+    void finish();
+
+    // Auxiliares de reproduccion
     int get_vol(const PlayerCar& car) const;
+    void try_play(const PlayerCar& car, std::list<SoundInfo>& info,
+                  SDL2pp::Chunk& sound);
 
    public:
-    Sound(SDL2pp::Mixer& mixer, Game& game);
+    Sound(SDL2pp::Mixer& mixer, Game& game, const CityName& city_name);
 
-    void crash(const PlayerCar& car) const;
-    void test_brake(const PlayerCar& car);
-    void test_finish(const PlayerCar& car);
+    // Metodos de control
+    void start();
+    void update();
 
     MAKE_FIXED(Sound)
 };
 
 class Game final : Connexion::Responder {
-    friend class Car;
-    friend class Screen;
-    friend class Sound;
-
    private:
     // Componentes de conexion y configuracion
     Api& api;
-    const PlayerId my_id;
+    const CityInfo& city_info;
+    const RaceInfo& race_info;
 
     // Componentes visuales y auditivos
     Screen screen;
     Sound sound;
-    Assets assets;
-
-    // Componentes de snapshot estatico
-    const CityInfo& city_info;
-    const RaceInfo& race_info;
-
-    std::list<BoundingBox> bridges;
-    std::list<BoundingBox> overpasses;
-    std::vector<BoundingBox> checkpoints;
-    size_t checkpoint_amount;
 
     // Componentes de snapshot de actualizacion
     std::mutex snapshot_mutex;
     GameSnapshot current_snapshot;
     void on_game_snapshot(const GameSnapshot&) override;
 
-    std::unordered_map<PlayerId, PlayerCar> cars;
-    std::list<NpcCar> npcs;
-    PlayerCar* my_car = nullptr;
-
-    float time_elapsed = 0;
-    float time_countdown = 0;
-    float time_left = 0;
-
     // Componentes de colisiones recibidas
-    Queue<CollisionEvent> collisions;
+    // Queue<CollisionEvent> collisions;
     void on_collision_event(const CollisionEvent& collision_event) override;
-
-    // Componentes de camara a usar
-    int cam_x = 0;
-    int cam_y = 0;
-    void set_camera(const PlayerCar& car);
 
     // Pasos de actualizacion
     bool send_events();
@@ -151,6 +144,37 @@ class Game final : Connexion::Responder {
          const GameSetUp& setup);
     ~Game();
 
+    // Componentes de snapshot estatico
+    const PlayerId my_id;
+
+    std::list<BoundingBox> bridges;
+    std::list<BoundingBox> overpasses;
+    std::vector<BoundingBox> checkpoints;
+    size_t checkpoint_amount;
+
+    // Componentes parseados de snapshot de actualizacion (visual)
+    std::unordered_map<PlayerId, PlayerCar> cars;
+    std::list<NpcCar> npcs;
+    PlayerCar* my_car = nullptr;
+
+    float time_elapsed = 0;
+    float time_countdown = 0;
+    float time_left = 0;
+
+    // Componentes parseados de snapshot de actualizacion (sonido)
+    Queue<CollisionEvent> collisions;
+
+    std::unordered_map<PlayerId, bool> old_braking;
+    std::unordered_map<PlayerId, bool> old_disqualified;
+    uint32_t old_checkpoint = 0;
+    bool old_finished = false;
+
+    // Componentes de camara a usar
+    int cam_x = 0;
+    int cam_y = 0;
+    void set_camera(const PlayerCar& car);
+
+    // Metodo de inicio.
     bool start();
 
     MAKE_FIXED(Game)
